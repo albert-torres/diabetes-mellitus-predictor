eee6e474e1700f11e4bdffea7af37b8678af4ac7
Albert <atorresrubio@outlook.es>
2021-12-18 20:10:40 +0100

    Fix log string

--- src/main.py
+++ src/main.py
@@ -31,14 +31,14 @@ measures_diabetes_df = DataframeTransformer.split_dataframe_first_measures(diabe
 measures_diabetes_df = DataframeTransformer.merge_dfs_on_column(measures_diabetes_df, symptoms_df, on='ID')
 measures_diabetes_df = DataframeTransformer.get_dummies(measures_diabetes_df, ['Sexo', 'symptoms'])
 measures_diabetes_df.dropna(inplace=True)
-print(f"N diabetes (diabetes=1): {measures_diabetes_df.size}")
+print(f'N diabetes (diabetes=1): {measures_diabetes_df.size}')
 print(measures_diabetes_df.head())
 
 measures_no_diabetes_df = DataframeTransformer.split_dataframe_last_measures(no_diabetes_df, 0)
 measures_no_diabetes_df = DataframeTransformer.merge_dfs_on_column(measures_no_diabetes_df, symptoms_df, on='ID')
 measures_no_diabetes_df = DataframeTransformer.get_dummies(measures_no_diabetes_df, ['Sexo', 'symptoms'])
 measures_no_diabetes_df.dropna(inplace=True)
-print(f"\nN no diabetes (diabetes=0): {measures_no_diabetes_df.size}")
+print(f'\nN no diabetes (diabetes=0): {measures_no_diabetes_df.size}')
 print(measures_no_diabetes_df.head())
 
 measures_df = pd.concat([measures_diabetes_df, measures_no_diabetes_df], ignore_index=True)
@@ -47,7 +47,7 @@ measures_df.drop(['ID'], axis='columns', inplace=True)
 # Save processed data
 DataPersister.save(measures_df, 'train_test_dataset_001.csv')
 
-print(f"\nProcessed dataset: {measures_no_diabetes_df.size}")
+print(f'\nProcessed dataset: {measures_no_diabetes_df.size}')
 print(measures_df.head())
 
 # Training
@@ -60,16 +60,16 @@ train_y = train.diabetes
 test_y = test.diabetes
 
 classifiers = [
-    (KNeighborsClassifier(), ""),
-    (SVC(kernel='linear'), "linear"),
-    (SVC(gamma=2), "gamma_2"),
-    (DecisionTreeClassifier(), ""),
-    (RandomForestClassifier(), ""),
-    (AdaBoostClassifier(), ""),
-    (GaussianNB(), ""),
-    (QuadraticDiscriminantAnalysis(), ""),
-    (SGDClassifier(), ""),
-    (MLPClassifier(), ""),
+    (KNeighborsClassifier(), ''),
+    (SVC(kernel='linear'), 'linear'),
+    (SVC(gamma=2), 'gamma_2'),
+    (DecisionTreeClassifier(), ''),
+    (RandomForestClassifier(), ''),
+    (AdaBoostClassifier(), ''),
+    (GaussianNB(), ''),
+    (QuadraticDiscriminantAnalysis(), ''),
+    (SGDClassifier(), ''),
+    (MLPClassifier(), ''),
 ]
 
 classifiers_scores = []
@@ -79,19 +79,22 @@ for model, description in classifiers:
 
     score = mean(cross_val_score(model, train_x, train_y))
     classifiers_scores.append(score)
-    print(f"\nModel: {ModelPersister.get_model_name(model)}")
-    print(f"Score: {score}")
+    log_string = f'\nModel: {ModelPersister.get_model_name(model)}'
+    if description:
+        log_string = f'{log_string} ({description})'
+    print(log_string)
+    print(f'Score: {score}')
 
 # Best model
 best_model_idx = classifiers_scores.index(max(classifiers_scores))
-best_model = classifiers[best_model_idx].fit(train_x, train_y)
+best_model = classifiers[best_model_idx][0].fit(train_x, train_y)
 pred_y = best_model.predict(test_x)
 print('\nBest model')
 print(f'Model: {ModelPersister.get_model_name(classifiers[best_model_idx])}')
 print(f'Test accuracy score: {accuracy_score(test_y, pred_y)}')
 print(f'Test AUC score: {roc_auc_score(test_y, pred_y)}')
 
-# tree.plot_tree(dtc_model, proportion=True, ax=ax, fontsize=10, max_depth=3, filled=True, feature_names=c, class_names=["0", "1"])
+# tree.plot_tree(dtc_model, proportion=True, ax=ax, fontsize=10, max_depth=3, filled=True, feature_names=c, class_names=['0', '1'])
 # plt.savefig('decision_tree.jpeg')
 # print(measures_diabetes_df.size)
 # print(measures_diabetes_df[:50])
