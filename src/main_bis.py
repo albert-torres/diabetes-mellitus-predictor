aa23b38b4be2b9515ff78a9cbd8ef06e8e083196
Albert <atorresrubio@outlook.es>
2021-12-18 18:17:46 +0100

    Add new models

--- src/main_bis.py
+++ src/main_bis.py
@@ -3,21 +3,13 @@ from collections import Counter
 from statistics import mean
 
 import pandas as pd
-from matplotlib import pyplot as plt
-from sklearn import tree
-from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import SGDClassifier
-from sklearn.metrics import accuracy_score, roc_auc_score
+from sklearn.metrics import accuracy_score
 from sklearn.model_selection import cross_val_score, train_test_split
-from sklearn.neural_network import MLPClassifier
+from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
-from sklearn.gaussian_process import GaussianProcessClassifier
-from sklearn.gaussian_process.kernels import RBF
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
-from sklearn.naive_bayes import GaussianNB
-from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 
 from src.readers import DatasetReader
 from src.transformers import DataframeTransformer
@@ -34,16 +26,19 @@ symptoms_df = DatasetReader.read_symptoms('/data/sintomas.csv')
 # Grouping (Erase groups that have not all Pruebas done?)
 measures_diabetes_df = DataframeTransformer.split_dataframe_first_measures(diabetes_df, 1)
 measures_diabetes_df = DataframeTransformer.merge_dfs_on_column(measures_diabetes_df, symptoms_df, on='ID')
-measures_diabetes_df = DataframeTransformer.get_dummies(measures_diabetes_df, ['Sexo', 'symptoms'])
-# measures_diabetes_df = DataframeTransformer.get_dummies(measures_diabetes_df, ['Sexo'])
+measures_diabetes_df = measures_diabetes_df[
+    (measures_diabetes_df['symptoms'] == 'Diabetes') |
+    (measures_diabetes_df['symptoms'] == 'Diabetes, sobrepeso')
+]
+measures_diabetes_df.drop('symptoms', axis='columns', inplace=True)
+measures_diabetes_df = DataframeTransformer.get_dummies(measures_diabetes_df, ['Sexo'])
 measures_diabetes_df.dropna(inplace=True)
 print(f"N diabetes (diabetes=1): {measures_diabetes_df.size}")
 print(measures_diabetes_df.head())
 
 measures_no_diabetes_df = DataframeTransformer.split_dataframe_first_measures(no_diabetes_df, 0)
-measures_no_diabetes_df = DataframeTransformer.merge_dfs_on_column(measures_no_diabetes_df, symptoms_df, on='ID')
-measures_no_diabetes_df = DataframeTransformer.get_dummies(measures_no_diabetes_df, ['Sexo', 'symptoms'])
-# measures_no_diabetes_df = DataframeTransformer.get_dummies(measures_no_diabetes_df, ['Sexo'])
+# measures_no_diabetes_df = DataframeTransformer.merge_dfs_on_column(measures_no_diabetes_df, symptoms_df, on='ID')
+measures_no_diabetes_df = DataframeTransformer.get_dummies(measures_no_diabetes_df, ['Sexo'])
 measures_no_diabetes_df.dropna(inplace=True)
 print(f"N no diabetes (diabetes=0): {measures_no_diabetes_df.size}")
 print(measures_no_diabetes_df.head())
@@ -54,46 +49,31 @@ measures_df.drop(['ID'], axis='columns', inplace=True)
 # print(measures_df.size)
 # print(measures_df.head())
 
-
-
 # Training
 
 train, test = train_test_split(measures_df, test_size=0.2, random_state=5, stratify=measures_df.diabetes)
 train_x = train.loc[:, train.columns != 'diabetes']
-train_x = StandardScaler().fit_transform(train_x)
 test_x = test.loc[:, train.columns != 'diabetes']
-test_x = StandardScaler().fit_transform(test_x)
 train_y = train.diabetes
 test_y = test.diabetes
 
-classifiers = [
-    KNeighborsClassifier(),
-    SVC(kernel='linear'),
-    SVC(gamma=2),
-    DecisionTreeClassifier(),
-    RandomForestClassifier(),
-    AdaBoostClassifier(),
-    GaussianNB(),
-    QuadraticDiscriminantAnalysis(),
-    SGDClassifier(),
-    MLPClassifier(),
-]
-
-classifiers_scores = []
-
-for model in classifiers:
-    score = mean(cross_val_score(model, train_x, train_y))
-    classifiers_scores.append(score)
+knn_model = KNeighborsClassifier(n_neighbors=3)
+gnb_model = GaussianNB()
+svc_model = SVC()
+stochastic_model = SGDClassifier()
+dtc_model = DecisionTreeClassifier()
+models = [knn_model, gnb_model, svc_model, stochastic_model, dtc_model]
+for model in models:
+    scores = cross_val_score(model, train_x, train_y)
     print(f"\nModel: {type(model).__name__}")
-    print(f"Score: {score}")
+    print(f"Score: {mean(scores)}")
 
-# Best model
-best_model_idx = classifiers_scores.index(max(classifiers_scores))
-best_model = classifiers[best_model_idx].fit(train_x, train_y)
-pred_y = best_model.predict(test_x)
-print(f'\nModel: {type(classifiers[best_model_idx]).__name__}')
-print(f'Test accuracy score: {accuracy_score(test_y, pred_y)}')
-print(f'Test AUC score: {roc_auc_score(test_y, pred_y)}')
+dtc_model = dtc_model.fit(train_x, train_y)
+
+# Test
+
+pred_y = dtc_model.predict(test_x)
+print(f'Test score: {accuracy_score(test_y, pred_y)}')
 
 # tree.plot_tree(dtc_model, proportion=True, ax=ax, fontsize=10, max_depth=3, filled=True, feature_names=c, class_names=["0", "1"])
 # plt.savefig('decision_tree.jpeg')
@@ -101,5 +81,4 @@ print(f'Test AUC score: {roc_auc_score(test_y, pred_y)}')
 # print(measures_diabetes_df[:50])
 # print(measures_diabetes_df.size)
 end = time.time()
-print()
 print(end - start)
